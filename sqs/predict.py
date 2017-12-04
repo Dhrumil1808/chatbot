c63103ca8ac5d1ae3e6880f15ea2189873cdc8bb
Rashmi Sharma <rashmishrm74@gmail.com>
2017-12-04 14:36:36 -0800

    changes for predictions

--- sqs/predict.py
+++ sqs/predict.py
@@ -17,7 +17,7 @@ def getModel():
     (trainX, trainY), (testX, testY), (validX, validY) = data_utils.split_dataset(idx_q, idx_a)
     train_batch_gen = data_utils.rand_batch_gen(trainX, trainY, 32)
 
-    print len(trainX)
+    #print len(trainX)
     test_batch_gen = data_utils.rand_batch_gen(testX, testY, 256)
     input_ = test_batch_gen.next()[0]
     xseq_len = 25
@@ -36,12 +36,12 @@ def getModel():
                                )
     sess = model.restore_last_session()
     output = model.predict(sess, input_[0:25])
-    print(output)
+    #print(output)
     return model, sess
 
 
 
-def chat(model,sess,question):
+def chat(question):
     # load data from pickle and npy files
     metadata, idx_q, idx_a = data.load_data(PATH='datasets/cornell_corpus/')
 
@@ -73,19 +73,14 @@ def chat(model,sess,question):
         response="I am sorry. I didn't understand that!"
     return response
 
+
+
 model, sess = getModel()
 
-
-
-chat(model,sess,"What do you think about life? Is life fair for you? You are a jerk though!")
-chat(model,sess, "well i really think hes got a chance")
-chat(model,sess,"my god these people are insane")
-chat(model,sess,"You never wanted to go out with 'me, did you?")
-chat(model,sess,"How do you get your hair to look like that?")
-chat(model,sess,"You think you ' re the only sophomore at the prom?")
+chat("What do you think about life? Is life fair for you? You are a jerk though!")
 
 
 while (True):
     the_input = raw_input("Enter input: ")
-    response  = chat(model,sess,the_input)
+    response  = chat(the_input)
     print response
